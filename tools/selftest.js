925e9ab4a68c177a26c5e4ae3edcee8ccbbbde30
Emily Stark <emily@meteor.com>
2014-08-01 11:40:35 -0700

    Grab `selftest.fail` from packaging branch

--- tools/selftest.js
+++ tools/selftest.js
@@ -545,7 +545,7 @@ _.extend(Sandbox.prototype, {
     };
     self.write(to, contents);
   },
-  
+
   // Delete a file in the sandbox. 'filename' is as in write().
   unlink: function (filename) {
     var self = this;
